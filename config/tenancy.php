17db668838ce607afbb81c4ff06ba004b708b60c
sameh <smta55@gmail.com>
2023-09-27 18:23:38 +0300

    modify tenant domain config

--- config/tenancy.php
+++ config/tenancy.php
@@ -18,7 +18,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'saas.test',
+        'talsystem.com',
         'saas.test', // Add the ones that you use. I use this one with Laravel Valet.
     ],
 
